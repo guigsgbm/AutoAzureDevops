327ea26a72cbb40e6808d93d5e8018d41b741950
Guilherme Braga Magalhães <guilherme.magalhaes@mkth.com.br>
2022-11-10 09:54:59 -0300

    update script

--- AddUser.sh
+++ AddUser.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export AZURE_DEVOPS_EXT_PAT=$(System.AccessToken)
+export AZURE_DEVOPS_EXT_PAT='$(System.AccessToken)'
 az devops login
 
 az devops user list --org https://dev.azure.com/guigsgbm
