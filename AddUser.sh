a467fafd8f51f0efed851d95fb0a0fa93cf44bac
Guilherme Braga Magalhães <guilherme.magalhaes@mkth.com.br>
2022-11-10 11:41:54 -0300

    update script

--- AddUser.sh
+++ AddUser.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+export AZURE_DEVOPS_EXT_PAT=$__PAT__
+az devops login
 env | grep '^__.*__'
 
-echo $__PAT__ | az devops login
 az devops user list --org https://dev.azure.com/guigsgbm/
 az devops logout
