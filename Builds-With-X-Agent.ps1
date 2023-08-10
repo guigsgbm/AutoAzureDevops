4416aba75b8b376d49606f4a8a0fea5156f407cb
Guilherme Magalhães <guigsgbm@gmail.com>
2023-08-10 11:16:35 -0300

    new script

---
+++ Builds-With-X-Agent.ps1
@@ -0,0 +1,45 @@
+function BasicAuthHeader {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$PAT
+    )
+
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAT)"))
+
+    $headers = @{
+    Authorization = "Basic $base64AuthInfo"
+    }
+
+    $headers
+}
+
+$PAT = "Personal Access Token"
+$orgName = "Organization Name"
+$agentPoolID = "Agent Pool ID"
+
+$headerdata = BasicAuthHeader "$PAT"
+$buildPipes = @()
+
+$runs = (Invoke-WebRequest `
+-Uri ("https://dev.azure.com/$orgName/_apis/distributedtask/pools/$agentPoolID/jobrequests?api-version=6.0") `
+-UseBasicParsing `
+-Headers $headerdata `
+-Method Get).Content | ConvertFrom-Json
+
+foreach ($run in $runs.value){
+
+    $buildPipe = [PSCustomObject]@{
+        Name = $run.definition.name
+        Url = $run.definition._links.web.href
+    }
+
+    if ($null -eq ($buildPipes | Where-Object { $_.Name -eq $buildPipe.Name -and $_.Url -eq $buildPipe.Url })) {
+            $buildPipes += $buildPipe
+        }
+}
+
+$uniqueBuildPipes = $buildPipes | Select-Object -Property Name, Url
+
+$json = $buildPipes | ConvertTo-Json
+
+$json | Out-File -FilePath "Builds-With-X-Agent.json"
